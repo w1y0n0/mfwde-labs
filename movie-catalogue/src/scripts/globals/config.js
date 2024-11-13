4f2510f5ebe97bf7e10a78df49d69eff57c998b7
w1y0n0 <wiyono30@gmail.com>
2024-11-13 11:27:33 +0700

    Latihan: Application Shell

---
+++ movie-catalogue/src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '45598d1da8a65b7b101904e08143e7aa',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
---
+++ movie-catalogue/webpack.prod.js
@@ -0,0 +1,23 @@
+import { merge } from 'webpack-merge';
+import common from './webpack.common';
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
