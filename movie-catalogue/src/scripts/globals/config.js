c15800ee6845204d88371882d93ab644156b203a
w1y0n0 <wiyono30@gmail.com>
2024-11-14 16:37:56 +0700

    Latihan: Web Socket

--- movie-catalogue/src/scripts/globals/config.js
+++ movie-catalogue/src/scripts/globals/config.js
@@ -4,6 +4,10 @@ const CONFIG = {
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+  WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
 };
 
 export default CONFIG;
--- movie-catalogue/webpack.common.js
+++ movie-catalogue/webpack.common.js
@@ -42,22 +42,6 @@ module.exports = {
     }),
     new WorkboxWebpackPlugin.GenerateSW({
       swDest: './sw.bundle.js',
-      runtimeCaching: [
-        {
-          urlPattern: ({ url }) => url.href.startsWith('https://api.themoviedb.org/3/'),
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'themoviedb-api',
-          },
-        },
-        {
-          urlPattern: ({ url }) => url.href.startsWith('https://image.tmdb.org/t/p/w500/'),
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'themoviedb-image-api',
-          },
-        },
-      ],
     }),
   ],
 };
