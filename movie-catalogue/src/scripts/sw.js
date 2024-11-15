c3754daa31fdcab33f7efef22ab1eb7e6cc29135
w1y0n0 <wiyono30@gmail.com>
2024-11-15 08:30:57 +0700

    Latihan: Ubah Build-Tools Menjadi InjectManifest (Workbox)

--- movie-catalogue/src/scripts/sw.js
+++ movie-catalogue/src/scripts/sw.js
@@ -1,32 +1,48 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import CacheHelper from './utils/cache-helper';
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute, Route } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
 
-// Daftar asset yang akan di-caching
-const assetsToCache = [
-  './',
-  './icons/icon-72x72.png',
-  './icons/icon-96x96.png',
-  './icons/icon-128x128.png',
-  './icons/icon-144x144.png',
-  './icons/icon-152x152.png',
-  './icons/icon-192x192.png',
-  './icons/icon-384x384.png',
-  './icons/icon-512x512.png',
-  './index.html',
-  './favicon.png',
-  './app.bundle.js',
-  './app.webmanifest',
-  './sw.bundle.js',
-];
+// Do precaching
+precacheAndRoute(self.__WB_MANIFEST);
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
+const themoviedbApi = new Route(
+  ({ url }) => url.href.startsWith('https://api.themoviedb.org/3/'),
+  new StaleWhileRevalidate({
+    cacheName: 'themoviedb-api',
+  }),
+);
+
+const themoviedbImageApi = new Route(
+  ({ url }) => url.href.startsWith('https://image.tmdb.org/t/p/w500/'),
+  new StaleWhileRevalidate({
+    cacheName: 'themoviedb-image-api',
+  }),
+);
+
+registerRoute(themoviedbApi);
+registerRoute(themoviedbImageApi);
+
+self.addEventListener('install', () => {
+  console.log('Service Worker: Installed');
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(CacheHelper.deleteOldCache());
-});
+self.addEventListener('push', (event) => {
+  console.log('Service Worker: Pushed');
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(CacheHelper.revalidateCache(event.request));
+  const notificationData = {
+    title: 'Push Notification',
+    options: {
+      body: 'This is a push notification',
+      icon: '/favicon.png',
+      image: '/icon-512x512/icon-512x512.jpg',
+    },
+  };
+
+  const showNotification = self.registration.showNotification(
+    notificationData.title,
+    notificationData.options,
+  );
+
+  event.waitUntil(showNotification);
 });
--- movie-catalogue/webpack.common.js
+++ movie-catalogue/webpack.common.js
@@ -40,7 +40,9 @@ module.exports = {
         },
       ],
     }),
-    new WorkboxWebpackPlugin.GenerateSW({
+
+    new WorkboxWebpackPlugin.InjectManifest({
+      swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
       swDest: './sw.bundle.js',
     }),
   ],
